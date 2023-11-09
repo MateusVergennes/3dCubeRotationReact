76754f8436d0d3f3235ce46ad17a1f9b12267505
MateusVergennes <mateusalpi@hotmail.com>
2023-11-08 23:33:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import { Canvas, useFrame, extend } from 'react-three-fiber';
+import { BoxGeometry, Mesh, MeshBasicMaterial, DoubleSide, PerspectiveCamera } from 'three';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+extend({ BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera });
+
+function Cube() {
+  const cubeRef = useRef();
+
+  useFrame(({ camera }) => {
+    cubeRef.current.rotation.x += 0.01;
+    cubeRef.current.rotation.y += 0.01;
+
+    // Ajustar a posição da câmera para manter o cubo visível
+    camera.position.z = 10;
+  });
+
+  return (
+    <mesh ref={cubeRef}>
+      <boxGeometry args={[6, 6, 6]} />
+      <meshBasicMaterial color="blue" side={DoubleSide} />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Cubo Girando</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <Canvas>
+            <Cube />
+          </Canvas>
+        </div>
+      </div>
     </div>
   );
 }
